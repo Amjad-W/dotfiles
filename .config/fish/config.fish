6551d63de8f4c20157f970552c3db1cc7a8cc000
Amjad W. <amjadshomali60@gmail.com>
2024-12-30 10:41:53 +0200

    Add fish & alacritty config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+fish_vi_key_bindings
+fish_config theme choose "Dracula Official"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+set --universal nvm_default_version v22.12.0
+set --universal nvm_default_packages yarn
